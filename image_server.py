de6b4f27afd8e7d7b0c3f4160fe450fe2f5b06cb
pupuis <luismueller2309@gmail.com>
2022-07-18 21:17:00 +0200

    Removes RawImage.json and loads schema from dict

--- image_server.py
+++ image_server.py
@@ -1,13 +1,61 @@
 import base64
 from functools import partial
 import json
-import os
 import time
 import click
 from lemon import entrypoint
 from lemon.api import subscribe
 from foxglove_websocket.server import FoxgloveServer
 import numpy as np
+
+RAW_IMAGE = {
+    "$comment": "Generated from RawImage by @foxglove/schemas",
+    "title": "RawImage",
+    "description": "A raw image",
+    "type": "object",
+    "properties": {
+        "timestamp": {
+            "type": "object",
+            "title": "time",
+            "properties": {
+                "sec": {
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "nsec": {
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 999999999
+                }
+            },
+            "description": "Timestamp of image"
+        },
+        "width": {
+            "type": "integer",
+            "minimum": 0,
+            "description": "Image width"
+        },
+        "height": {
+            "type": "integer",
+            "minimum": 0,
+            "description": "Image height"
+        },
+        "encoding": {
+            "type": "string",
+            "description": "Encoding of the raw image data"
+        },
+        "step": {
+            "type": "integer",
+            "minimum": 0,
+            "description": "Byte length of a single row"
+        },
+        "data": {
+            "type": "string",
+            "contentEncoding": "base64",
+            "description": "Raw image data"
+        }
+    }
+}
 
 def to_raw_image(img: "np.ndarray", timestamp):
     encoding = 'mono8' if len(img.shape) < 3 else 'rgb8'
@@ -25,15 +73,14 @@ def to_raw_image(img: "np.ndarray", timestamp):
 @entrypoint
 async def start(topics):
     async with FoxgloveServer("127.0.0.1", 8765, "Image Server for Lemon 🍋") as server:
-        with open(os.path.dirname(os.path.realpath(__file__)) + '/RawImage.json', 'r') as f:
-            schema = json.dumps(json.load(f))
-            channels = {
-                topic: await server.add_channel({
-                    "topic": topic,
-                    "encoding": "json",
-                    "schemaName": "foxglove.RawImage",
-                    "schema": schema
-                }) for topic in topics}
+        schema = json.dumps(RAW_IMAGE)
+        channels = {
+            topic: await server.add_channel({
+                "topic": topic,
+                "encoding": "json",
+                "schemaName": "foxglove.RawImage",
+                "schema": schema
+            }) for topic in topics}
 
         async def send(topic, msg):
             channel = channels[topic]
